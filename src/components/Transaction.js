0566a3aac97a41101ca087756b686706d3425e18
pjmantoss <joelptoss@gmail.com>
2020-04-25 17:00:57 +0100

    update Transaction

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -3,7 +3,9 @@ import React from 'react'
 export const Transaction = () => {
     return (
         <div>
-            
+            <li>
+                {transaction.text} <span>-$400</span><button className="delete-btn">x</button>
+            </li>
         </div>
     )
 }
