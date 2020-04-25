fab3383b41d37ca1d9fb98b6ce32e1f60422a6b2
pjmantoss <joelptoss@gmail.com>
2020-04-25 15:47:00 +0100

    add TransactionList component

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
       <Header />
       <div className="container">
         <Balance />
+        <IncomeExpenses />
       </div>
     </div>
   );
---
+++ src/components/TransactionList.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const TransactionList = () => {
+    return (
+        <div>
+            <h3></h3>
+            <ul>
+                Cash <span>$400</span><button className="delete-btn">x</button>
+            </ul>
+        </div>
+    )
+}
