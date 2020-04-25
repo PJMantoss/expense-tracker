68fd9d00b8ca1f13361f2805e624d628c45e612d
pjmantoss <joelptoss@gmail.com>
2020-04-25 15:31:03 +0100

    update App.js and add Balance component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,9 @@
 import React from 'react'
 
-const Header = () => {
+export const Header = () => {
     return (
         <h1>
             Expense Tracker
         </h1>
     )
 }
-
-export default Header
