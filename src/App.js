b5e1034b82ba76cb9adfe1ce3e3027a39a5ba9c3
pjmantoss <joelptoss@gmail.com>
2020-04-25 19:36:34 +0100

    add footer info to App.js

--- src/App.js
+++ src/App.js
@@ -17,6 +17,11 @@ function App() {
         <TransactionList/>
         <AddTransaction/>
       </div>
+      <div id="love">
+          <span>Made With</span>
+          <img src="https://img.icons8.com/dusk/64/000000/like.png" alt="love"/> 
+          <span>by <a href="https://github.com/PJMantoss" target="_blank">Mantoss</a></span>
+      </div>
     </GlobalProvider>
   );
 }
