bb44aa372301b3d995f15fd4c591a29acbcf2405
pjmantoss <joelptoss@gmail.com>
2020-04-25 16:40:39 +0100

    update GlobalState context

--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -9,3 +9,19 @@ const initialState = {
         {id: 4, text: 'Camera', amount: 150}
     ]
 }
+
+//Create context
+export const GlobalContext = createContext(initialState);
+
+//Provider component
+export const GlobalProvider = ({ children }) => {
+    const [state, dispatch] = useReducer(AppReducer, initialState);
+
+    return (
+        <GlobalContext.Provider value={{
+            transaction: state.transaction 
+        }}>
+            {children}
+        </GlobalContext.Provider>
+    )
+}
