e6352df954b47d922c426251e420f22c4e85dd4c
pjmantoss <joelptoss@gmail.com>
2020-04-25 19:29:32 +0100

    update AppReducer

--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -7,7 +7,7 @@ export const TransactionList = () => {
 
     return (
         <div>
-            <h3></h3>
+            <h3>History</h3>
             <ul className="list">
                 {
                     transactions.map(transaction => (<Transaction key={transaction.id} transaction={transaction} />))
