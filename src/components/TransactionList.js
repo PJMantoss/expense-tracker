4013e3cdb31ac9713d7e4bbd5bc959dbeb43218b
pjmantoss <joelptoss@gmail.com>
2020-04-25 16:50:31 +0100

    update TransactionList

--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -1,6 +1,9 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { GlobalContext } from '../context/GlobalState'
 
 export const TransactionList = () => {
+    const { transaction } = useContext(GlobalContext);
+
     return (
         <div>
             <h3></h3>
