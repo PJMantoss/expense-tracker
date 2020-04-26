1745dfa203afdd98abb8048c520cff4e8835e46a
pjmantoss <joelptoss@gmail.com>
2020-04-26 15:44:11 +0100

    Updates

---
+++ precache-manifest.27226a60517bde62acde5f2a7b0ce1de.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8031e0ff59e21212d023f00ad9c59257",
+    "url": "/expense-tracker/index.html"
+  },
+  {
+    "revision": "e56041153836ec294c05",
+    "url": "/expense-tracker/static/css/main.4717b452.chunk.css"
+  },
+  {
+    "revision": "02d65ad6ec437f72acf1",
+    "url": "/expense-tracker/static/js/2.070f1458.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/expense-tracker/static/js/2.070f1458.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e56041153836ec294c05",
+    "url": "/expense-tracker/static/js/main.308ab4c9.chunk.js"
+  },
+  {
+    "revision": "6331dbe230cc9389428a",
+    "url": "/expense-tracker/static/js/runtime-main.4baf4d3a.js"
+  }
+]);
